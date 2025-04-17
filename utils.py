702543ff37470150c466d347de1cbc2576eb0046
Jugz-08 <70230128+Jugz-08@users.noreply.github.com>
2025-04-17 19:00:07 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,7 @@
 from openai import OpenAI
 import os
-from dotenv import load_dotenv
 import base64
 import streamlit as st
-
-load_dotenv()
 
 client = OpenAI(
     api_key=st.secrets["OPENAI_API_KEY"]
